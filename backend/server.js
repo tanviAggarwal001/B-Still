ef1451660b732c73948df7df7158f52cbdacd6a8
Shivam Singh <2023uai1819@mnit.ac.in>
2025-02-12 22:14:43 +0530

    fix the minor mistake in deployed backend url

--- backend/server.js
+++ backend/server.js
@@ -33,5 +33,5 @@ app.get("/", (req, res) => {
 
 // Start Server
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://https://b-still-backend.onrender.com:${PORT}`);
+  console.log(`🚀 Server running on https://b-still-backend.onrender.com:${PORT}`);
 });
--- my-app/src/pages/Login.js
+++ my-app/src/pages/Login.js
@@ -31,7 +31,7 @@ const LoginPage = () => {
     }
 
     try {
-      const url = "http://https://b-still-backend.onrender.com:5000/auth/login";
+      const url = "https://b-still-backend.onrender.com/auth/login";
       const response = await fetch(url, {
         method: "POST",
         headers: {
