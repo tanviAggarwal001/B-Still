33298a5d20ab40374a67d908902a95d8f78301c0
Shivam Singh <2023uai1819@mnit.ac.in>
2025-02-16 16:14:25 +0530

    fix2 'create a resume 'link

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -41,7 +41,7 @@ function Home() {
       case "Create":
         return <h2><Create/></h2>; // Replace with your Create component
       case "Your Work":
-        return <h2><YourWork/></h2>; // Replace with your Your Work component
+        return <YourWork setActiveTab={setActiveTab} />;
       case "Check ATS Score":
         return <h2><CheckATS/></h2>; // Replace with your Check ATS Score component
       case "Tutorials":
--- my-app/src/pages/YourWork.js
+++ my-app/src/pages/YourWork.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
 
-
-function YourWork() {
+function YourWork({ setActiveTab }) {
     const [resumes, setResumes] = useState([]);
 
   useEffect(() => {
